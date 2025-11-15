43e5184ba021bb9a93edeed066166b4e270f476d
Philipp Pracht <research@philipp-pracht.de>
2025-11-15 23:18:07 +0100

    switch around expected and calculated in assert

--- tests/utils/test_rdkit_io.py
+++ tests/utils/test_rdkit_io.py
@@ -18,7 +18,7 @@ def test_get_cn_rdkit(caffeine_cn_test_data):
     caffeine_rdkit = Chem.MolFromXYZBlock(caffeine_xyz)
 
     cn = get_cn_rdkit(caffeine_rdkit)
-    assert pytest.approx(cn, abs=1e-6) == expected_cn
+    assert pytest.approx(expected_cn, abs=1e-6) == cn
 
 
 def test_get_axis_rdkit(caffeine_axis_test_data):
@@ -27,9 +27,9 @@ def test_get_axis_rdkit(caffeine_axis_test_data):
     caffeine_rdkit = Chem.MolFromXYZBlock(caffeine_xyz)
 
     rot, avmom, evec = get_axis_rdkit(caffeine_rdkit)
-    assert pytest.approx(rot, abs=1e-6) == expected_rot
-    assert pytest.approx(avmom, abs=1e-6) == expected_avmom
-    assert pytest.approx(evec, abs=1e-6) == expected_evec
+    assert pytest.approx(expected_rot, abs=1e-6) == rot
+    assert pytest.approx(expected_avmom, abs=1e-6) == avmom
+    assert pytest.approx(expected_evec, abs=1e-6) == evec
 
 
 def test_get_canonical_rdkit(caffeine_canonical_test_data):
@@ -38,7 +38,7 @@ def test_get_canonical_rdkit(caffeine_canonical_test_data):
     caffeine_rdkit = Chem.MolFromXYZBlock(caffeine_xyz)
 
     canonical = get_canonical_rdkit(caffeine_rdkit, heavy=heavy)
-    assert pytest.approx(canonical, abs=1e-6) == expected_canonical
+    assert pytest.approx(expected_canonical, abs=1e-6) == canonical
 
 
 def test_get_rmsd_rdkit(caffeine_rmsd_test_data):
@@ -51,9 +51,9 @@ def test_get_rmsd_rdkit(caffeine_rmsd_test_data):
     expected_aligned_mol = Chem.MolFromXYZBlock(caffeine_aligned)
 
     rmsd, aligned_mol, Umat = get_rmsd_rdkit(caffeine_rdkit_1, caffeine_rdkit_2)
-    assert pytest.approx(rmsd, abs=1e-6) == expected_rmsd
-    assert pytest.approx(Umat, abs=1e-6) == expected_Umat
+    assert pytest.approx(expected_rmsd, abs=1e-6) == rmsd
+    assert pytest.approx(expected_Umat, abs=1e-6) == Umat
     assert (
-        pytest.approx(aligned_mol.GetConformer().GetPositions(), abs=1e-6)
-        == expected_aligned_mol.GetConformer().GetPositions()
+        pytest.approx(expected_aligned_mol.GetConformer().GetPositions(), abs=1e-6)
+        == aligned_mol.GetConformer().GetPositions()
     )
