e47b6b22e8bd0a17facc972189041fa2ff403706
Tobias Kaczun <tobias@kaczun.de>
2025-11-18 14:20:42 +0100

    refactor data, add cmds.py tests

--- tests/fixtures/data.py
+++ tests/fixtures/data.py
@@ -1,3 +1,8 @@
+from random import shuffle
+
+import numpy as np
+import pytest
+
 CAFFEINE_XTB = """24
 
 C          1.07317        0.04885       -0.07573
@@ -250,3 +255,236 @@ H      2.891210    -5.063621    -0.060902
 H      4.418204    -5.200748     0.829363
 H      4.416983    -5.208472    -0.952145
 """
+
+CAFFEINE_RMSD_TEST_DATA = [
+    # Conformer1, Conformer2, heavy, rmsd, aligned_conformer, Umat
+    (
+        CAFFEINE_XTB,
+        CAFFEINE_XTB_ROTATED,
+        False,
+        0.00000042,
+        CAFFEINE_XTB,
+        # FIXME: check with crest, this is actually taken from this code itself but the rmsd is the same as crest
+        np.asarray(
+            [
+                [0.000586, 0.025178, 0.999683],
+                [0.025178, 0.999366, -0.025185],
+                [-0.999683, 0.025185, -0.000049],
+            ],
+            dtype=np.float64,
+        ),
+    ),
+    (
+        CAFFEINE_XTB,
+        CAFFEINE_OBABEL,
+        False,
+        0.13939435,
+        CAFFEINE_OBABEL_ALIGNED,
+        # FIXME: check with crest, this is actually taken from this code itself but the rmsd is the same as crest
+        np.asarray(
+            [
+                [0.165823, -0.986155, -0.000907],
+                [0.986155, 0.165823, 0.000503],
+                [-0.000346, -0.000978, 0.999999],
+            ],
+            dtype=np.float64,
+        ),
+    ),
+    (
+        CAFFEINE_XTB,
+        CAFFEINE_XTB_ROTATED,
+        True,
+        0.00000042,
+        CAFFEINE_XTB,
+        # FIXME: check with crest, this is actually taken from this code itself but the rmsd is the same as crest
+        np.asarray(
+            [
+                [0.000586, 0.025178, 0.999683],
+                [0.025178, 0.999366, -0.025185],
+                [-0.999683, 0.025185, -0.000049],
+            ],
+            dtype=np.float64,
+        ),
+    ),
+    (
+        CAFFEINE_XTB,
+        CAFFEINE_OBABEL,
+        True,
+        0.1725815540,
+        CAFFEINE_OBABEL_HEAVY_ALIGNED,
+        # FIXME: check with crest, this is actually taken from this code itself but the rmsd is the same as crest
+        np.asarray(
+            [
+                [0.171404, -0.9852, -0.001332],
+                [0.985201, 0.171405, -0.00012],
+                [0.000347, -0.001292, 0.999999],
+            ],
+            dtype=np.float64,
+        ),
+    ),
+]
+
+
+CAFFEINE_AXIS_TEST_DATA = [
+    # Conformer1, rot constants in MHz, avmom in a.u., evec
+    (
+        CAFFEINE_XTB,
+        # FIXME: check with crest, this is actually taken from this code itself but the rmsd is the same as crest
+        np.asarray(
+            [1068.073127, 710.711798, 430.26121],
+            dtype=np.float64,
+        ),
+        1.30564546e-44,
+        np.asarray(
+            [
+                [0.671158, -0.741313, 0.001206],
+                [0.741314, 0.671157, -0.001149],
+                [0.000042, 0.001665, 0.999999],
+            ],
+            dtype=np.float64,
+        ),
+    ),
+    (
+        CAFFEINE_OBABEL,
+        # FIXME: check with crest, this is actually taken from this code itself but the rmsd is the same as crest
+        np.asarray(
+            [1146.303928, 700.554317, 438.439373],
+            dtype=np.float64,
+        ),
+        1.28135123e-44,
+        np.asarray(
+            [
+                [0.657967, 0.753046, -0.000501],
+                [0.753044, -0.657967, -0.002205],
+                [-0.00199, 0.001073, -0.999997],
+            ],
+            dtype=np.float64,
+        ),
+    ),
+]
+
+CAFFEINE_CN_TEST_DATA = [
+    # Conformer1, cn
+    (
+        CAFFEINE_OBABEL,
+        # FIXME: check with crest, this is actually taken from this code itself but the rmsd is the same as crest
+        np.asarray(
+            # fmt: off
+            [3.689705,3.866219,2.934731,3.626334,3.144874,3.791101,2.768159,0.858364,2.743387,
+             2.716063,0.860503,2.747386,3.698299,3.698564,0.924137,0.924083,0.924084,0.925654,
+             0.924202,0.92413,0.924141,0.924245,0.924105,0.924098],
+            # fmt: on
+            dtype=np.float64,
+        ),
+    ),
+]
+
+CAFFEINE_CANONICAL_TEST_DATA = [
+    # Conformer1, heavy, canonical_atom_id
+    (
+        CAFFEINE_XTB,
+        False,
+        # FIXME: check with crest, this is actually taken from this code itself but the rmsd is the same as crest
+        np.asarray(
+            # fmt: off
+                [1,8,5,7,13,14,12,6,10,9,2,11,4,3,15,15,15,18,17,17,17,16,16,16],
+            # fmt: on
+            dtype=int,
+        ),
+    ),
+    (
+        CAFFEINE_XTB,
+        True,
+        # FIXME: check with crest, this is actually taken from this code itself but the rmsd is the same as crest
+        np.asarray(
+            # fmt: off
+                [1,8,5,7,13,14,12,6,10,9,2,11,4,3,0,0,0,0,0,0,0,0,0,0],
+            # fmt: on
+            dtype=int,
+        ),
+    ),
+]
+
+CAFFEINE_IRMSD_TEST_DATA = [
+    # Conformer1, Conformer2, mask, rmsd, aligned_conformer, Umat
+    (
+        CAFFEINE_XTB,
+        CAFFEINE_XTB,
+        0.00000000,
+        CAFFEINE_XTB,
+    ),
+    (
+        CAFFEINE_XTB,
+        CAFFEINE_XTB_ROTATED,
+        0.00000042,
+        CAFFEINE_XTB,
+    ),
+    (CAFFEINE_XTB, CAFFEINE_GFF, 0.06160473, CAFFEINE_GFF_ALIGNED),
+]
+
+
+@pytest.fixture(scope="session")
+def caffeine_molecule_xyz():
+    """Fixture that returns a caffeine molecule."""
+    return CAFFEINE_XTB
+
+
+@pytest.fixture(scope="session")
+def caffeine_molecule_xyz_rotated():
+    """Fixture that returns a caffeine molecule."""
+    return CAFFEINE_XTB_ROTATED
+
+
+@pytest.fixture(scope="session")
+def caffeine_molecule_xyz_obabel():
+    """Fixture that returns a caffeine molecule."""
+    return CAFFEINE_OBABEL
+
+
+@pytest.fixture(scope="session", params=CAFFEINE_RMSD_TEST_DATA)
+def caffeine_rmsd_test_data(request):
+    """Fixture that returns atom numbers and positions for caffeine molecule in
+    different conformations."""
+    return request.param
+
+
+@pytest.fixture(
+    scope="session",
+    params=CAFFEINE_AXIS_TEST_DATA,
+)
+def caffeine_axis_test_data(request):
+    return request.param
+
+
+@pytest.fixture(
+    scope="session",
+    params=CAFFEINE_CN_TEST_DATA,
+)
+def caffeine_cn_test_data(request):
+    return request.param
+
+
+@pytest.fixture(
+    scope="session",
+    params=CAFFEINE_CANONICAL_TEST_DATA,
+)
+def caffeine_canonical_test_data(request):
+    return request.param
+
+
+@pytest.fixture(
+    scope="session",
+    params=CAFFEINE_IRMSD_TEST_DATA,
+)
+def caffeine_irmsd_test_data(request):
+    conformer1, conformer2, expected_irmsd, expected_aligned_conformer = request.param
+    conformer2_list = conformer2.splitlines()
+    conformer2_header = conformer2_list[:2]
+    conformer2_molecule = conformer2_list[2:]
+    shuffle(conformer2_molecule)
+
+    # conformer2_shuffled = conformer2_header + conformer2_molecule[::-1]
+    conformer2_shuffled = conformer2_header + conformer2_molecule
+
+    conformer2 = "\n".join(conformer2_shuffled)
+    return conformer1, conformer2, expected_irmsd, expected_aligned_conformer
