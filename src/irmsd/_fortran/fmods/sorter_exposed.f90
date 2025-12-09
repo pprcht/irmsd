4f04b10b1dee7c4a41ed7a152e0494a56de8c17e
Philipp Pracht <research@philipp-pracht.de>
2025-12-10 00:58:31 +0100

    cont'd reimplementation of classical CREGEN routine

--- src/irmsd/_fortran/fmods/sorter_exposed.f90
+++ src/irmsd/_fortran/fmods/sorter_exposed.f90
@@ -576,6 +576,273 @@ contains
 
   end subroutine delta_irmsd_list_fortran
 
+  subroutine cregen_classic_sort(nall,structures,groups,rthresh,ethr,bthr, &
+      &                         printlvl)
+!**************************************************************************************
+!* Re-implementaiton of the CREGEN workflow, classifying conformers
+!* according to their quaternion RMSD, rotational constants, and energy
+!
+!* Input arguments:
+!*        nall - total number of structures
+!*  structures - the structures
+!*      rthresh - RMSD threshold (in ANGSTRÖM) for conformer distinction
+!*        ethr - inter-conformer energy threshold (in HARTREE) for pre-sorting
+!*        bthr - rotational constant similarity threshold (percentage based)
+!*
+!* Optionals:
+!*    printlvl - integer to direct the print verbosity. (0=minimal, 1=verbose)
+!*
+!* Output:
+!*      groups - integer array assigning each structure to a group
+!*************************************************************************************
+    implicit none
+    !> INPUT
+    integer,intent(in) :: nall
+    type(coord),intent(inout),target :: structures(nall)
+    integer,intent(inout) :: groups(nall)
+    real(wp),intent(in) :: RTHRESH
+    real(wp),intent(in) :: ETHR
+    real(wp),intent(in) :: BTHR
+    integer,intent(in),optional  :: printlvl
+
+    !> LOCAL
+    integer :: i,ii,jj,T,cc,nat,io
+    integer :: gcount
+    integer :: prlvl
+    type(rmsd_cache),allocatable :: rcaches(:)
+    type(coord),allocatable,target :: workmols(:)
+    type(canonical_sorter),allocatable :: sorters(:)
+    type(coord),pointer :: ref,mol
+    real(wp) :: rmsdval,RTHR,ediff,eii,avmom
+    real(wp),allocatable :: rot(:,:)
+    integer,allocatable :: prune_table(:)
+    integer,allocatable :: topo_group(:)
+    logical :: individual_IDs
+
+    logical,parameter :: debug = .false.
+
+!>--- handle optional arguments
+    if (present(printlvl)) then
+      prlvl = printlvl
+    else
+      prlvl = 1
+    end if
+
+!>--- set up parallelization
+!     ...
+    T = 1 !> doing it serial for now
+
+!>--- set up parameters (NOTE, we are working with BOHR internally)
+    RTHR = RTHRESH*aatoau
+
+!>--- print some sorting data
+    if (prlvl > 0) then
+      write (stdout,'(a)') 'Info for CREGEN sorting:'
+      write (stdout,'(2x,a,i10)') 'number of structures     :',nall
+      write (stdout,'(2x,a,f10.5,a)') 'RTHR (RMSD threshold)    :',RTHR*autoaa,' Å'
+      write (stdout,'(2x,a,es10.2,a)') 'ETHR (energy threshold)  :',ETHR,' Ha'
+      write (stdout,'(2x,a,f10.2,a)') 'BTHR (rot. threshold)    :',BTHR*100,' %'
+
+      !write (stdout,'(2x,a,i9)') 'OpenMP threads           :',T
+      write (stdout,*)
+    end if
+
+!>--- allocate work cache
+    if (prlvl > 0) then
+      write (stdout,'(a)',advance='no') 'Allocating RMSD work cache ... '
+      flush (stdout)
+    end if
+    allocate (rcaches(T))
+    ref => structures(1)
+    nat = ref%nat
+    allocate (workmols(T))
+    do i = 1,T
+      mol => workmols(i)
+      allocate (mol%at(ref%nat))
+      allocate (mol%xyz(3,ref%nat))
+      nullify (mol)
+      call rcaches(i)%allocate(ref%nat)
+    end do
+    if (prlvl > 0) then
+      write (stdout,'(a)') 'done.'
+      write (stdout,*)
+    end if
+
+!> ----------------------------------------------
+!> PRE-PROCESSING for more efficient sorting
+!> ----------------------------------------------
+    !> prune_table keeps track of which structure to compare to
+    !> so for a  list of structures (1...j...k...nall), the entry
+    !> prune_table(k) = j, tells us structure k is compared to all
+    !> structures j up to k-1. The table is initialized to 1, so
+    !> the full comparison list is used.
+    allocate (prune_table(nall),source=1)
+    !> conveniently, we can use the energy threshold to set a better
+    !> comparison table, as in the original CREGEN routine.
+    do ii = 1,nall
+      eii = structures(ii)%energy
+      do jj = 1,ii
+        ediff = abs(eii-structures(jj)%energy)
+        if (ediff <= ETHR) then
+          prune_table(ii) = jj
+          exit
+        end if
+      end do
+    end do
+
+    !> Prepare axis comparison
+    !> axis alignment and rotational constant calculation
+    allocate (rot(3,nall),source=0.0_wp)
+    do ii = 1,nall
+      mol => structures(ii)
+      call axis(mol%nat,mol%at,mol%xyz) !> all coordinates to CMA
+      call axis(mol%nat,mol%at,moL%xyz*autoaa,rot(1:3,ii),avmom)!> B_0 in MHz
+    end do
+
+    !> topo_group dynamically keeps track of different topology groups
+    !> among the structures. All structures start out as the same topology
+    allocate (topo_group(nall),source=1)
+!> --------------------------------------------
+!> pre-processing end
+!> --------------------------------------------
+
+!>--- run the checks
+    gcount = maxval(groups(:))
+    do ii = 1,nall
+!>--- find next unassigned conformer and assign a new group
+      if (groups(ii) .ne. 0) cycle
+      gcount = gcount+1
+      groups(ii) = gcount
+
+!>--- Then, cross-check all other unassigned conformers
+      cc = 1  !> again, serial implementation for now
+      ! !$omp parallel &
+      ! !$omp shared(nall, nat, groups, individual_IDs, sorters, rcaches) &
+      ! !$omp shared(workmols, structures, ii, prune_table,topo_group) &
+      ! !$omp private(jj,rmsdval,cc,io)
+      ! !$omp do schedule(dynamic)
+      do jj = ii+1,nall
+        !cc = omp_get_thread_num()+1
+        if (groups(jj) .ne. 0) cycle
+        if (ii < prune_table(jj)) cycle
+        if (topo_group(ii) .ne. topo_group(jj)) cycle
+        workmols(cc)%nat = structures(jj)%nat
+        workmols(cc)%at(:) = structures(jj)%at(:)
+        workmols(cc)%xyz(:,:) = structures(jj)%xyz(:,:)
+        rmsdval = rmsd(structures(ii),workmols(cc), &
+          &       scratch=rcaches(cc)%xyzscratch, ccache=rcaches(cc)%ccache)
+!        if (io .ne. 0) then
+!          topo_group(jj) = topo_group(jj)+1
+!          cycle
+!        end if
+        if (rmsdval < RTHR) groups(jj) = gcount
+      end do
+      ! !$omp end do
+      ! !$omp end parallel
+    end do
+
+    if (debug) then
+      write (*,*) 'assigned groups, and count'
+      do ii = 1,maxval(groups(:))
+        write (*,*) ii,count(groups(:) == ii)
+      end do
+      if (.not.all(topo_group(:) == 1)) then
+        write (*,*) 'assigned topology groups and count'
+        do ii = 1,maxval(topo_group(:))
+          write (*,*) ii,count(topo_group(:) == ii)
+        end do
+      end if
+    end if
+
+    if (allocated(topo_group)) deallocate (topo_group)
+    if (allocated(prune_table)) deallocate (prune_table)
+  end subroutine cregen_classic_sort
+
+  subroutine cregen_exposed_fortran( &
+  &                     nat,nall,xyzall_ptr,atall_ptr, &
+  &                     groups_ptr,rthresh,ethr,bthr,printlvl, &
+  &                     energies_ptr &
+  &                   ) bind(C,name="cregen_exposed_fortran")
+    use,intrinsic :: iso_c_binding
+    implicit none
+
+    ! C-facing arguments
+    integer(c_int),value :: nat
+    integer(c_int),value :: nall
+    type(c_ptr),value :: xyzall_ptr
+    type(c_ptr),value :: atall_ptr
+    type(c_ptr),value :: groups_ptr
+    real(c_double),value :: rthresh
+    real(c_double),value :: ethr 
+    real(c_double),value :: bthr 
+    integer(c_int),value :: printlvl
+    type(c_ptr),value :: energies_ptr
+
+    ! Fortran pointer views of C buffers
+    real(c_double),pointer :: xyzall(:)
+    integer(c_int),pointer :: atall(:)
+    integer(c_int),pointer :: groups(:)
+    real(c_double),pointer :: energies(:)
+
+    ! Local Fortran variables
+    type(coord),allocatable :: structures(:)
+    integer :: i,j,k1,k2,l
+    logical :: allcanon
+
+    ! Map raw C pointers to Fortran pointers with shapes
+    call c_f_pointer(xyzall_ptr,xyzall, [3*nat*nall])
+    call c_f_pointer(atall_ptr,atall, [nat*nall])
+    call c_f_pointer(groups_ptr,groups, [nall])
+    call c_f_pointer(energies_ptr,energies, [nall])
+
+    ! Call original Fortran routine
+    allocate (structures(nall))
+    k1 = 0
+    k2 = 0
+    do i = 1,nall
+      structures(i)%nat = nat
+      allocate (structures(i)%at(nat),source=0)
+      allocate (structures(i)%xyz(3,nat),source=0.0_wp)
+      structures(i)%energy = energies(i)
+      do j = 1,nat
+        k1 = k1+1
+        structures(i)%at(j) = atall(k1)
+        do l = 1,3
+          k2 = k2+1
+          structures(i)%xyz(l,j) = xyzall(k2)*aatoau  !> Angström to BOHR
+        end do
+      end do
+    end do
+
+    !> init groups to zero (no assignment)
+    groups(1:nall) = 0
+
+    !> call the actual routine
+    call cregen_classic_sort(nall,structures,groups,rthresh,ethr,bthr, &
+      &                      printlvl=printlvl)
+
+    !> coordinates for each structure have been aligned and sorted,
+    !> so we need to pass them back
+    k1 = 0
+    k2 = 0
+    do i = 1,nall
+      do j = 1,nat
+        k1 = k1+1
+        atall(k1) = structures(i)%at(j)
+        do l = 1,3
+          k2 = k2+1
+          xyzall(k2) = structures(i)%xyz(l,j)*autoaa
+        end do
+      end do
+    end do
+
+    !> free memory
+    deallocate (structures)
+
+    !> group assignment (== unique conformers) on "groups" array
+
+  end subroutine cregen_exposed_fortran
+
 !=============================================================================!
 !#############################################################################!
 !=============================================================================!
